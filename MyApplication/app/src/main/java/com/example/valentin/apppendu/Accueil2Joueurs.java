ca489b4ff8f02e6aca8e010285e9a1f0dc0d3467
sylvainbenard <sylvain.benard@iut-rodez.fr>
2017-02-02 11:07:21 +0100

    Accueil 2 Joueur fini Navigation Bouton

--- MyApplication/app/src/main/java/com/example/valentin/apppendu/Accueil2Joueurs.java
+++ MyApplication/app/src/main/java/com/example/valentin/apppendu/Accueil2Joueurs.java
@@ -1,7 +1,9 @@
 package com.example.valentin.apppendu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.NumberPicker;
 
 public class Accueil2Joueurs extends AppCompatActivity {
@@ -15,5 +17,14 @@ public class Accueil2Joueurs extends AppCompatActivity {
         nbMots.setMinValue(1);
         nbMots.setMaxValue(100);
         nbMots.setWrapSelectorWheel(true);
+    }
+
+    public void pagePrecedente(View view){
+        Intent intent = new Intent(Accueil2Joueurs.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void pageSuivante(View view){
+
     }
 }
--- MyApplication/app/src/main/java/com/example/valentin/apppendu/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/valentin/apppendu/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.valentin.apppendu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void page2Joueur(View view){
+        Intent intent = new Intent(MainActivity.this, Accueil2Joueurs.class);
+        startActivity(intent);
+    }
+
+    public void parametres(View view){
+        Intent intent = new Intent(MainActivity.this, GestionCategories.class);
+        startActivity(intent);
     }
 }
