bd4c1742e51f07871bdc8a2c403e67c1f2576985
Quentin-974 <kentyn-440@hotmail.fr>
2017-02-03 11:20:39 +0100

    Accueil2Joueurs => Passage des noms et du nombre de coup à l'activité suivante Mots2Joueurs => Récupération des valeurs et initialisation des champs

--- MyApplication/app/src/main/java/com/example/valentin/apppendu/Activity/Accueil2Joueurs.java
+++ MyApplication/app/src/main/java/com/example/valentin/apppendu/Activity/Accueil2Joueurs.java
@@ -4,18 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 
 import com.example.valentin.apppendu.R;
 
 public class Accueil2Joueurs extends AppCompatActivity {
 
+    /** Identifiant pour le passage du nom du joueur1 */
+    public final static String NOM_JOUEUR1 = "JOUEUR1";
+    /** Identifiant pour le passage du nom du joueur2 */
+    public final static String NOM_JOUEUR2 = "JOUEUR2";
+    /** Identifiant pour le passage du nombre de coup de la partie */
+    public final static String NBCOUPS = "COUPS";
+
+    /**  */
     private NumberPicker nbMots;
+    /** EditText pour le joueur 1 */
+    private EditText et_joueur1;
+    /** EditText pour le joueur 2 */
+    private EditText et_joueur2;
+
+    /**Nombre de coup de la partie */
+    private int nbCoup;
+    /** Nom du joueur 1 */
+    private String joueur1;
+    /** Nom du joueur 2 */
+    private String joueur2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil2_joueur);
+        // Récupération des accès sur les différents widgets
         nbMots = (NumberPicker) findViewById(R.id.nombreMots);
+        et_joueur1 = (EditText) findViewById(R.id.nomJoueur1);
+        et_joueur2 = (EditText) findViewById(R.id.nomJoueur2);
+
+        // Initialisation du numberPicker
         nbMots.setMinValue(1);
         nbMots.setMaxValue(100);
         nbMots.setWrapSelectorWheel(true);
@@ -27,6 +54,16 @@ public class Accueil2Joueurs extends AppCompatActivity {
     }
 
     public void pageSuivante(View view){
-
+        // Récupération du nombre de coup
+        nbCoup = nbMots.getValue();
+        // Récupération des noms des joueur
+        joueur1 = et_joueur1.getText().toString();
+        joueur2 = et_joueur2.getText().toString();
+        // Passage des valeurs à la prochaine activité
+        Intent intent = new Intent(Accueil2Joueurs.this, Mots2Joueurs.class);
+        intent.putExtra(NBCOUPS, nbCoup);
+        intent.putExtra(NOM_JOUEUR1, joueur1);
+        intent.putExtra(NOM_JOUEUR2, joueur2);
+        startActivity(intent);
     }
 }
--- MyApplication/app/src/main/java/com/example/valentin/apppendu/Activity/Mots2Joueurs.java
+++ MyApplication/app/src/main/java/com/example/valentin/apppendu/Activity/Mots2Joueurs.java
@@ -20,9 +20,12 @@ public class Mots2Joueurs extends Activity {
     private ImageButton btnPrecedent;
     /** Bouton suivant */
     private ImageButton btnSuivant;
-
     /** Nombre de coup saisie*/
-    private int nbCoup = 7;
+    private int nbCoup;
+    /** Nom du joueur 1*/
+    private String joueur1;
+    /** Nom du joueur 2*/
+    private String joueur2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +38,23 @@ public class Mots2Joueurs extends Activity {
         btnPrecedent = (ImageButton) findViewById(R.id.imgPrecedentMots);
         btnSuivant = (ImageButton) findViewById(R.id.imgSuivantMots);
 
+        // Récupération des valeurs par intent
+        Bundle extras = getIntent().getExtras();
+            if (extras != null ) {
+                nbCoup = extras.getInt(Accueil2Joueurs.NBCOUPS);
+                joueur1 = extras.getString(Accueil2Joueurs.NOM_JOUEUR1);
+                joueur2 = extras.getString(Accueil2Joueurs.NOM_JOUEUR2);
+            }
+
+        // initialisation de la TextView
+        nomJoueur.setText(joueur1);
+
+        // Vider le conteneur au préalable
         conteneur.removeAllViews();
+        // Création des editText dynamiquement
         for (int i = 0; i < nbCoup; i++) {
             EditText motSaisie = (EditText) getLayoutInflater().inflate(R.layout.editext,null);
+            motSaisie.setHint("Mot " + (i+1));
             conteneur.addView(motSaisie);
         }
     }
